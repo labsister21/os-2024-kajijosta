58fbe32c077b4e9fb05f39dcbc0c3945ca112592
satriadhikara <satriadhikara@gmail.com>
2024-03-05 09:20:16 +0700

    refactor: fix unnecessary bit field

--- src/header/cpu/gdt.h
+++ src/header/cpu/gdt.h
@@ -36,8 +36,8 @@ extern struct GDTR _gdt_gdtr;
 struct SegmentDescriptor
 {
     // First 32-bit
-    uint16_t segment_low : 16;
-    uint16_t base_low : 16;
+    uint16_t segment_low;
+    uint16_t base_low;
 
     // Next 16-bit (Bit 32 to 47)
     uint8_t base_mid;
