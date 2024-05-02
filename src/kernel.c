120a771d2e0588ab5fca3fe04c2f37437fd632de
andhikafdh <andhikafadhillah2211@gmail.com>
2024-05-02 19:07:37 +0700

    feat: add paging.h

--- src/kernel.c
+++ src/kernel.c
@@ -4,30 +4,71 @@
 #include "header/cpu/idt.h"
 #include "header/cpu/interrupt.h"
 #include "header/cpu/keyboard.h"
+#include "header/cpu/fat32.h"
+#include "header/cpu/disk.h"
 #include <stdbool.h>
+
+
+// void kernel_setup(void)
+// {
+//     load_gdt(&_gdt_gdtr);
+//     pic_remap();
+//     initialize_idt();
+//     activate_keyboard_interrupt();
+//     framebuffer_clear();
+//     framebuffer_set_cursor(0, 0);
+//     keyboard_state_activate();
+//     while (true)
+//     {
+//         char c;
+//         int row, col;
+//         bool print_mode;
+//         get_keyboard_buffer(&c, &row, &col, &print_mode);
+
+//         if (c && print_mode)
+//         {
+//             col -= 1;
+//             framebuffer_write(row, col, c, 0xF, 0);
+//         }
+
+//         framebuffer_set_cursor(row, col);
+//     }
+// }
 
 void kernel_setup(void)
 {
-    load_gdt(&_gdt_gdtr);
-    pic_remap();
-    initialize_idt();
-    activate_keyboard_interrupt();
-    framebuffer_clear();
-    framebuffer_set_cursor(0, 0);
-    keyboard_state_activate();
-    while (true)
-    {
-        char c;
-        int row, col;
-        bool print_mode;
-        get_keyboard_buffer(&c, &row, &col, &print_mode);
+  load_gdt(&_gdt_gdtr);
+  pic_remap();
+  activate_keyboard_interrupt();
+  initialize_idt();
+  framebuffer_clear();
+  framebuffer_set_cursor(0, 0);
+  initialize_filesystem_fat32();
+  // struct BlockBuffer b;
+  // for (int i = 0; i < 512; i++)
+  //     b.buf[i] = i % 16;
+  // write_blocks(&b, 17, 1);
+  // struct BlockBuffer b;
+  // read_blocks(&b, 17, 1);
+  struct ClusterBuffer buf;
+  struct FAT32DriverRequest req2 = {.buf = &buf,
+                                    .name = "kano",
+                                    .parent_cluster_number = 0x02,
+                                    .buffer_size = 10000};
+  uint8_t err = delete(req2);
+  if (err == 0)
+    framebuffer_write(0, 0, '0', 0xF, 0);
+  else if (err == 1)
+    framebuffer_write(0, 0, '1', 0xF, 0);
+  else if (err == 2)
+    framebuffer_write(0, 0, '2', 0xF, 0);
+  else
+  {
+    framebuffer_write(0, 0, '-', 0xF, 0);
+    framebuffer_write(0, 1, '1', 0xF, 0);
+  }
 
-        if (c && print_mode)
-        {
-            col -= 1;
-            framebuffer_write(row, col, c, 0xF, 0);
-        }
-
-        framebuffer_set_cursor(row, col);
-    }
+  while (true)
+    ;
+  // Write file
 }
