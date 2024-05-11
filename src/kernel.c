be4a4af1c0a25ecc0f71779fff647d138b8bf7bb
attaramajesta <attaraayub@gmail.com>
2024-05-11 16:32:10 +0700

    feat: add user mode

--- src/kernel.c
+++ src/kernel.c
@@ -10,33 +10,35 @@
 #include <stdbool.h>
 
 
-void kernel_setup(void)
-{
-    // Test paging
-    paging_allocate_user_page_frame(&_paging_kernel_page_directory, (uint8_t*) 0x600000);
-    *((uint8_t*) 0x500000) = 1;
+void kernel_setup(void) {
     load_gdt(&_gdt_gdtr);
     pic_remap();
     initialize_idt();
     activate_keyboard_interrupt();
     framebuffer_clear();
     framebuffer_set_cursor(0, 0);
-    keyboard_state_activate();
-    while (true)
-    {
-        char c;
-        int row, col;
-        bool print_mode;
-        get_keyboard_buffer(&c, &row, &col, &print_mode);
+    initialize_filesystem_fat32();
+    gdt_install_tss();
+    set_tss_register();
 
-        if (c && print_mode)
-        {
-            col -= 1;
-            framebuffer_write(row, col, c, 0xF, 0);
-        }
+    // Allocate first 4 MiB virtual memory
+    paging_allocate_user_page_frame(&_paging_kernel_page_directory, (uint8_t*) 0);
 
-        framebuffer_set_cursor(row, col);
-    }
+    // Write shell into memory
+    struct FAT32DriverRequest request = {
+        .buf                   = (uint8_t*) 0,
+        .name                  = "shell",
+        .ext                   = "\0\0\0",
+        .parent_cluster_number = ROOT_CLUSTER_NUMBER,
+        .buffer_size           = 0x100000,
+    };
+    read(request);
+
+    // Set TSS $esp pointer and jump into shell 
+    set_tss_kernel_current_stack();
+    kernel_execute_user_program((uint8_t*) 0);
+
+    while (true);
 }
 
 // void kernel_setup(void)
